750ae43b5a11f208f116b0ee8d0b086b87df65a2
Rohit <72918089+RohitSaini7@users.noreply.github.com>
2022-08-12 13:22:25 +0530

    add via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Home from "./pages/Home/Home";
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Home />
+    </React.Fragment>
+  );
+}
+
+export default App;
